8b2689febd081396424000cfefeccc6ba7fe5368
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-28 02:06:58 +0100

    Convert to arrow function

--- RecursiveFibonacci.js
+++ RecursiveFibonacci.js
@@ -1,5 +1,2 @@
-function fibsRec(n) {
-  return (n <= 3) ? [0, 1, 1] : [...fibsRec(n - 1), fibsRec(n - 2)[fibsRec(n - 2).length - 1] + fibsRec(n - 1)[fibsRec(n - 1).length - 1]];
-}
-
+const fibsRec = (n) => (n <= 3) ? [0, 1, 1] : [...fibsRec(n - 1), fibsRec(n - 2)[fibsRec(n - 2).length - 1] + fibsRec(n - 1)[fibsRec(n - 1).length - 1]];
 console.log(fibsRec(8));
