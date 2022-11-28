76869b02362c8556e06f0296fcac66350ac7959f
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-28 01:45:36 +0100

    Fix reversed output

--- RecursiveFibonacci.js
+++ RecursiveFibonacci.js
@@ -1,5 +1,5 @@
 function fibsRec(n) {
-  return (n <= 3) ? [1, 1, 0] : [fibsRec(n - 1)[0] + fibsRec(n - 2)[0]].concat(fibsRec(n - 1));
+  return (n <= 3) ? [0, 1, 1] : [...fibsRec(n - 1), fibsRec(n - 2)[fibsRec(n - 2).length - 1] + fibsRec(n - 1)[fibsRec(n - 1).length - 1]];
 }
 
 console.log(fibsRec(8));
