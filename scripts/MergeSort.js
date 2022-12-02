50f88a0e5f180d9b50f92ff1a42321922a113c70
Zekumoru Dragonhart <dev@zekumoru.com>
2022-12-02 12:35:56 +0100

    Export MergeSort

--- scripts/MergeSort.js
+++ scripts/MergeSort.js
@@ -1,30 +1,28 @@
-function mergeSort(array) {
-  if (array.length <= 1) return array;
+module.exports = class MergeSort {
+  static sort(array) {
+    if (array.length <= 1) return array;
 
-  // Dividing process
-  const middleIndex = Math.floor(array.length / 2);
-  const leftPart = mergeSort(array.slice(0, middleIndex));
-  const rightPart = mergeSort(array.slice(middleIndex, array.length));
+    // Dividing process
+    const middleIndex = Math.floor(array.length / 2);
+    const leftPart = MergeSort.sort(array.slice(0, middleIndex));
+    const rightPart = MergeSort.sort(array.slice(middleIndex, array.length));
 
-  // Conquering (merging) process
-  let leftPointer = 0;
-  let rightPointer = 0;
-  while (leftPointer < leftPart.length || rightPointer < rightPart.length) {
-    const index = leftPointer + rightPointer;
+    // Conquering (merging) process
+    let leftPointer = 0;
+    let rightPointer = 0;
+    while (leftPointer < leftPart.length || rightPointer < rightPart.length) {
+      const index = leftPointer + rightPointer;
 
-    if (leftPart[leftPointer] < rightPart[rightPointer] || rightPointer >= rightPart.length) {
-      array[index] = leftPart[leftPointer];
-      leftPointer++;
+      if (leftPart[leftPointer] < rightPart[rightPointer] || rightPointer >= rightPart.length) {
+        array[index] = leftPart[leftPointer];
+        leftPointer++;
+      }
+      else {
+        array[index] = rightPart[rightPointer];
+        rightPointer++;
+      }
     }
-    else {
-      array[index] = rightPart[rightPointer];
-      rightPointer++;
-    }
+
+    return array;
   }
-
-  return array;
-}
-
-const input = [3, 6, 7, 2, 8, 1, 4, 5];
-console.log(`Before sort: ${input}`);
-console.log(`After sort: ${mergeSort(input)}`);
+};
